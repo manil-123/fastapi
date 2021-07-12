f675aaa841029592b00eb1e971f51a5f32d49dab
manil-123 <punmanil3@gmail.com>
2021-07-12 10:15:28 +0545

    main modified

--- main.py
+++ main.py
@@ -20,35 +20,39 @@ soup = BeautifulSoup(htmlContent, 'html.parser')
 # print(soup.prettify())
 
 # Step 3: HTML tree traversal
-tradingData = soup.find(id='live-trading')
-rows = tradingData.find_all('tr')
-dictList = []
-for row in rows:
-    myDict = {}
-    data = row.find_all('td')
-    anchor = row.find_all('a')
-    for a in anchor:
-        for i in range(len(data)):
-            if i == 1 and a.get_text()!='':
-                myDict[a.get_text()] = data[i].get_text()
-    dictList.append(myDict)
-dictList.pop(0)
+dict_list = []
+
+
+def get_data():
+    trading_data = soup.find(id='live-trading')
+    rows = trading_data.find_all('tr')
+    for row in rows:
+        my_dict = {}
+        data = row.find_all('td')
+        anchor = row.find_all('a')
+        for a in anchor:
+            for i in range(len(data)):
+                if i == 1 and a.get_text()!='':
+                    my_dict[a.get_text()] = data[i].get_text()
+        dict_list.append(my_dict)
+    dict_list.pop(0)
 
 # print(dictList)
 # json_object = json.dumps(dictList, indent=4)
 # print(json_object)
+
 
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    return dictList
+    return dict_list
 
 
-schedule.every(5).seconds.do(index)
-schedule.every().hour.do(index)
-schedule.every().day.at("11:00").do(index)
+schedule.every(5).seconds.do(get_data)
+schedule.every().hour.do(get_data)
+schedule.every().day.at("11:00").do(get_data)
 
 while 1:
     schedule.run_pending()
